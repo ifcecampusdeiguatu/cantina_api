6c516bf1fcf1a3bdaf47505f8e580d3f65c912bc
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-05-13 00:55:25 -0300

    refactor: removendo importação sem uso

--- src/main/java/com/br/ifce/cantina/models/LancheNoite.java
+++ src/main/java/com/br/ifce/cantina/models/LancheNoite.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.persistence.JoinColumn;
 
