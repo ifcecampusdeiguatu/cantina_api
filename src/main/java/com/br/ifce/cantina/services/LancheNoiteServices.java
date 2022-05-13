4ba3356379c7297bfc6c60481c42128a831bbcff
Nalisson Gomes <nalissongm.pro@gmail.com>
2022-05-13 00:31:13 -0300

    refactor: arrumando importações

--- src/main/java/com/br/ifce/cantina/services/LancheNoiteServices.java
+++ src/main/java/com/br/ifce/cantina/services/LancheNoiteServices.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.br.ifce.cantina.models.Alimento;
 import com.br.ifce.cantina.models.LancheNoite;
 import com.br.ifce.cantina.repository.LancheNoiteRepository;
 
