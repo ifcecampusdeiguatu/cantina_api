a0d6b86185157ee6fe47c6579be642dc90d1bee0
Nalisson Gomes <nalissongm.pro@gmail.com>
2023-11-09 20:37:55 -0300

    test: update status

---
+++ src/modules/checkin/useCases/updateStatus/UpdateStatusUseCase.spec.ts
@@ -0,0 +1,180 @@
+import "reflect-metadata";
+import { ICheckinRepository } from "@modules/checkin/repositories/ICheckinRepository";
+import { CheckinRepositoryInMemory } from "@modules/checkin/repositories/in-memory/CheckinRepositoryInMemory";
+import { IMealsRepository } from "@modules/meal/repositories/IMealsRepository";
+import { MealsRepositoryInMemory } from "@modules/meal/repositories/in-memory/MealRepositoryInMemory";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+import { DayjsProvider } from "@shared/container/providers/DateProvider/implementations/DayjsProvider";
+import { AppError } from "@shared/errors/AppError";
+
+import { UpdateStatusUseCase } from "./UpdateStatusUseCase";
+
+let updateStateUseCase: UpdateStatusUseCase;
+let dayjsProvider: IDateProvider;
+let checkinRepositoryInMemoryUseCase: ICheckinRepository;
+let mealsRepositoryInMemory: IMealsRepository;
+
+const dateNow = new Date();
+
+const date = {
+  year: dateNow.getFullYear(),
+  month: dateNow.getMonth() + 1,
+  day: dateNow.getDate(),
+};
+
+describe("Update Status UseCase", () => {
+  beforeAll(() => {
+    checkinRepositoryInMemoryUseCase = new CheckinRepositoryInMemory();
+    mealsRepositoryInMemory = new MealsRepositoryInMemory();
+    dayjsProvider = new DayjsProvider();
+    updateStateUseCase = new UpdateStatusUseCase(
+      checkinRepositoryInMemoryUseCase,
+      mealsRepositoryInMemory,
+      dayjsProvider
+    );
+  });
+
+  it("should be possible to cancel a checkin", async () => {
+    const month = String(date.month).padStart(2, "0");
+    const day = String(date.day + 3).padStart(2, "0");
+
+    const schedule = `2023-${month}-${day}T11:30:00.000Z`;
+    const expiresDate = `2023-${month}-${day}T02:59:59.000Z`;
+
+    const meal = await mealsRepositoryInMemory.create({
+      schedule: new Date(schedule),
+      dishId: "dishFakeID",
+      localId: "localFakeID",
+    });
+
+    const checkin = await checkinRepositoryInMemoryUseCase.create({
+      status: "reserved",
+      expiresDate: new Date(expiresDate),
+      mealId: meal.id,
+      userId: "userFakeID",
+    });
+
+    await updateStateUseCase.execute({
+      id: checkin.id,
+      status: "canceled",
+      user: {
+        id: "userFakeID",
+        type: "aluno",
+      },
+    });
+
+    const checkinUpdated = await checkinRepositoryInMemoryUseCase.findById(
+      checkin.id
+    );
+
+    expect(checkinUpdated.status).toEqual("canceled");
+  });
+
+  it("should not be able to cancel lunch and morning snack after 6am on the day of the meal", async () => {
+    const month = String(date.month).padStart(2, "0");
+    const day = String(date.day).padStart(2, "0");
+
+    const schedule = `2023-${month}-${day}T11:30:00.000Z`;
+    const expiresDate = `2023-${month}-${day}T02:59:59.000Z`;
+    const date_start = `2023-${month}-${day}T09:10:00.000Z`;
+
+    const meal = await mealsRepositoryInMemory.create({
+      schedule: new Date(schedule),
+      dishId: "dishFakeID",
+      localId: "localFakeID",
+    });
+
+    const checkin = await checkinRepositoryInMemoryUseCase.create({
+      status: "reserved",
+      expiresDate: new Date(expiresDate),
+      mealId: meal.id,
+      userId: "userFakeID",
+    });
+
+    await expect(
+      updateStateUseCase.execute(
+        {
+          id: checkin.id,
+          status: "canceled",
+          user: {
+            id: "userFakeID",
+            type: "aluno",
+          },
+        },
+        new Date(date_start)
+      )
+    ).rejects.toEqual(new AppError("Cancellation deadline has expired"));
+  });
+
+  it("should not be able to cancel Afternoon Snack after 11am on the day of the meal", async () => {
+    const month = date.month.toString().padStart(2, "0");
+    const day = String(date.day).padStart(2, "0");
+    const date_start = `2023-${month}-${day}T14:10:00.000Z`;
+
+    const schedule = `2023-${month}-${day}T18:30:00.000Z`;
+    const expiresDate = `2023-${month}-${day}T02:59:59.000Z`;
+
+    const meal = await mealsRepositoryInMemory.create({
+      schedule: new Date(schedule),
+      dishId: "dishFakeID",
+      localId: "localFakeID",
+    });
+
+    const checkin = await checkinRepositoryInMemoryUseCase.create({
+      status: "reserved",
+      expiresDate: new Date(expiresDate),
+      mealId: meal.id,
+      userId: "userFakeID",
+    });
+
+    await expect(
+      updateStateUseCase.execute(
+        {
+          id: checkin.id,
+          status: "canceled",
+          user: {
+            id: "userFakeID",
+            type: "aluno",
+          },
+        },
+        new Date(date_start)
+      )
+    ).rejects.toEqual(new AppError("Cancellation deadline has expired"));
+  });
+
+  it("should not be able to cancel Night Snack after 16pm on the day of the meal", async () => {
+    const month = date.month.toString().padStart(2, "0");
+    const day = String(date.day).padStart(2, "0");
+    const date_start = `2023-${month}-${day}T21:10:00.000Z`;
+
+    const schedule = `2023-${month}-${day}T23:30:00.000Z`;
+    const expiresDate = `2023-${month}-${day}T02:59:59.000Z`;
+
+    const meal = await mealsRepositoryInMemory.create({
+      schedule: new Date(schedule),
+      dishId: "dishFakeID",
+      localId: "localFakeID",
+    });
+
+    const checkin = await checkinRepositoryInMemoryUseCase.create({
+      status: "reserved",
+      expiresDate: new Date(expiresDate),
+      mealId: meal.id,
+      userId: "userFakeID",
+    });
+
+    await expect(
+      updateStateUseCase.execute(
+        {
+          id: checkin.id,
+          status: "canceled",
+          user: {
+            id: "userFakeID",
+            type: "aluno",
+          },
+        },
+        new Date(date_start)
+      )
+    ).rejects.toEqual(new AppError("Cancellation deadline has expired"));
+  });
+});
--- src/modules/checkin/useCases/updateStatus/UpdateStatusUseCase.ts
+++ src/modules/checkin/useCases/updateStatus/UpdateStatusUseCase.ts
@@ -1,8 +1,10 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICheckinRepository } from "@modules/checkin/repositories/ICheckinRepository";
+import { IMealsRepository } from "@modules/meal/repositories/IMealsRepository";
 import { Status } from "@prisma/client";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
-import { inject, injectable } from "tsyringe";
 
 interface IRequest {
   id: string;
@@ -10,7 +12,7 @@ interface IRequest {
   user: {
     id: string;
     type: string;
-  }
+  };
 }
 
 @injectable()
@@ -18,31 +20,67 @@ export class UpdateStatusUseCase {
   constructor(
     @inject("CheckinRepository")
     private checkinRepository: ICheckinRepository,
+    @inject("MealsRepository")
+    private mealsRepository: IMealsRepository,
     @inject("DayjsProvider")
-    private dayjsProvider: IDateProvider,
-  ){}
+    private dayjsProvider: IDateProvider
+  ) {}
 
-  async execute({id, status, user}: IRequest){
+  async execute({ id, status, user }: IRequest, date_start?: Date) {
     const checkin = await this.checkinRepository.findById(id);
+    const now = date_start || new Date();
+    const isBefore = (init: Date, end: Date) =>
+      this.dayjsProvider.compareIfBefore(init, end);
 
-    if(!checkin){
+    if (!checkin) {
       throw new AppError("Checkin not found", 404);
     }
 
-    if(checkin.status === "canceled"){
-      throw new AppError("Check in was canceled");
+    const meal = await this.mealsRepository.findMealById(checkin.mealId);
+
+    const dayDiff = this.dayjsProvider.compareInDays(now, meal.schedule);
+
+    if (dayDiff <= 0 && status === "canceled") {
+      const cancelSchedules = [
+        this.dayjsProvider.setHours(meal.schedule, {
+          hours: 6,
+        }),
+        this.dayjsProvider.setHours(meal.schedule, {
+          hours: 11,
+        }),
+        this.dayjsProvider.setHours(meal.schedule, {
+          hours: 16,
+        }),
+      ];
+
+      const mealTime = meal.schedule.getUTCHours();
+
+      // +3 timezone offset
+      const isMorning = mealTime < 13 + 3;
+      const isAfternoon = mealTime >= 13 + 3 && mealTime < 18 + 3;
+      const isNight = mealTime >= 18 + 3;
+
+      const isNotCanCancel =
+        (isMorning && !isBefore(now, cancelSchedules[0])) ||
+        (isAfternoon && !isBefore(now, cancelSchedules[1])) ||
+        (isNight && !isBefore(now, cancelSchedules[2]));
+
+      if (isNotCanCancel) {
+        throw new AppError("Cancellation deadline has expired", 400);
+      }
     }
 
-    if((status === "done" || status === "lacked") && user.type !== "servidor"){
+    if (
+      (status === "done" || status === "lacked") &&
+      user.type !== "servidor"
+    ) {
       throw new AppError("User needs to be a Servidor");
     }
 
-    if(!this.dayjsProvider.compareIfBefore(new Date(), checkin.expiresDate)){
-      await this.checkinRepository.updateStatus({id, status:"lacked"});
-    }else {
-      await this.checkinRepository.updateStatus({id, status});
+    if (!this.dayjsProvider.compareIfBefore(now, checkin.expiresDate)) {
+      await this.checkinRepository.updateStatus({ id, status: "lacked" });
+    } else {
+      await this.checkinRepository.updateStatus({ id, status });
     }
-
-
   }
 }
